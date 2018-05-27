0c774b3dadc47e29bcb5c7def997cfe69354bb2e
MatobaSeiki <e1b15097@st.oit.ac.jp>
2018-05-27 21:33:58 +0900

    追加

--- functions.py
+++ functions.py
@@ -12,37 +12,24 @@ def softmax(a):
 
 
 def cross_entropy_error(y, t):
-    delta = 1e-7
-    return -np.sum(t * np.log(y + delta))
+    if y.ndim == 1:
+        t = t.reshape(1, t.size)
+        y = y.reshape(1, y.size)
+
+    # 教師データがone-hot-vectorの場合、正解ラベルのインデックスに変換
+    if t.size == y.size:
+        t = t.argmax(axis=1)
+
+    batch_size = y.shape[0]
+    return -np.sum(np.log(y[np.arange(batch_size), t] + 1e-7)) / batch_size
 
 
 def sigmoid(x):
     return 1 / (1 + np.exp(-x))
 
 
-# 偏微分
-def numerical_gradient(f, x):
-    h = 1e-4
-    grad = np.zeros_like(x)
-
-    for idx in range(x.size):
-        tmp_val = x[idx]
-        # f(x+h)の計算
-        x[idx] = tmp_val + h
-        fxh1 = f(x)
-
-        # f(x-h)の計算
-        x[idx] = tmp_val - h
-        fxh2 = f(x)
-
-        grad[idx] = (fxh1 - fxh2) / (2*h)
-        x[idx] = tmp_val  # 値を元に戻す
-
-    return grad
-
-
 # 偏微分の改良
-def _numerical_gradient(f, x):
+def numerical_gradient(f, x):
     h = 1e-4
     grad = np.zeros_like(x)
 
---
+++ train_nueralnet.py
@@ -0,0 +1,36 @@
+#!/usr/bin/env python3.6
+# -*- coding: utf-8 -*-
+
+import numpy as np
+from dataset.mnist import load_mnist
+from TwoLayerNet import TwoLayerNet
+
+(x_train, t_train), (x_test, y_test) = load_mnist(normalize=True)
+
+train_loss_list = []
+
+# ハイパーパラメータ
+iters_num = 10000
+train_size = 100
+batch_size = 100
+learning_rate = 0.1
+
+network = TwoLayerNet(input_size=784, hidden_size=50, output_size=10)
+
+for i in range(iters_num):
+    # ミニバッチの取得
+    batch_mask = np.random.choice(train_size, batch_size)
+    x_batch = x_train[batch_mask]
+    t_batch = t_train[batch_mask]
+
+    # 勾配の計算
+    grad = network.numerical_gradient(x_batch, t_batch)
+
+    # パラメータの更新
+    for key in ('W1', 'b1', 'W2', 'b2'):
+        network.params[key] -= learning_rate * grad[key]
+
+    # 学習経過の記錄
+    loss = network.loss(x_batch, t_batch)
+    print(loss)
+    train_loss_list.append(loss)
