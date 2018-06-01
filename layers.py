a627c9d917c7dc9871d09e577e36bc5c909641e0
MATOBAD <e1b15097@st.oit.ac.jp>
2018-06-01 02:16:37 -0700

    レイヤの追加

---
+++ layers.py
@@ -0,0 +1,87 @@
+#!/usr/bin/env python
+# -*- coding: utf-8 -*-
+import numpy as np
+from functions import softmax, cross_entropy_error
+import copy
+
+
+class Relu:
+    def __init__(self):
+        self.mask = None
+
+    def forward(self, x):
+        self.mask = (x <= 0)
+        out = copy.copy(x)
+        out[self.mask] = 0
+
+        return out
+
+    def backward(self, dout):
+        dout[self.mask] = 0
+        dx = dout
+
+        return dx
+
+
+class Sigmoid:
+    def __init__(self):
+        self.out = None
+
+    def forward(self, x):
+        out = 1 / (1 + np.exp(-x))
+        self.out = out
+
+        return out
+
+    def backward(self, dout):
+        dx = dout * (1.0 - self.out) * self.out
+
+        return dx
+
+
+class Affine:
+    def __init__(self, W, b):
+        self.W = W
+        self.b = b
+        self.x = None
+        self.dW = None
+        self.db = None
+
+    def forward(self, x):
+        self.x = x
+        out = np.dot(x, self.W) + self.b
+
+    def backward(self, dout):
+        dx = np.dot(dout, self.W.T)
+        self.dww = np.dot(self.x.T, dout)
+        self.db = np.sum(dout, axis=0)
+
+        return dx
+
+
+class SoftmaxWithLoss:
+    def __init__(self):
+        self.loss = None  # 損失
+        self.y = None  # softmaxの出力
+        self.t = None  # 教師データ(one-hot vector)
+
+    def forward(self, x, t):
+        self.t = t
+        self.y = softmax(x)
+        self.loss = cross_entropy_error(self.y, self.t)
+
+        return self.loss
+
+    def backward(self, dout=1):
+        batch_size = self.t.shape[0]
+        dx = (self.y - self.t) / batch_size
+
+        return dx
+
+if __name__ == '__main__':
+    relu = Relu()
+    print('relu=', relu.backward(np.array([[-2, 3], [2, -12]])))
+    sigmoid = Sigmoid()
+    print(sigmoid.forward(100))
+    softmaxwithloss = SoftmaxWithLoss()
+    print(softmaxwithloss.forward(np.array([1, 2]), np.array([0, 1])))
